549e35b279065b0fbc0869f0571a1aaf839cd5b5
Rob Debbane <studio@robertdebbane.com>
2016-11-18 11:12:13 -0500

    improved performance and cleaned up accounting_region_import

--- scripts/accounting_region_import.js
+++ scripts/accounting_region_import.js
@@ -12,17 +12,18 @@ var pool = mysql.createPool( {
   connectionLimt: 10
 });
 
+var selectData = [];
 // console.log(path.join(__dirname, process.argv[2]));
 pool.query('SET FOREIGN_KEY_CHECKS=0')
 .then(function() {
   return pool.query('truncate table accounting_region');
 })
 .then(function() {
-  fsp.readFile(path.join(__dirname, process.argv[2]), 'ascii')
+  return fsp.readFile(path.join(__dirname, process.argv[2]), 'ascii')
   .then(function(data) {
-    var lines = data.split('\n');
+    var lines = data.split('\r\n');
     lines.forEach(function(line, idx) {
-      //console.log(line);
+
       var lineData = line.split('\t');
       if (lineData.length === 8) {
         var prefix = lineData[0],
@@ -39,22 +40,24 @@ pool.query('SET FOREIGN_KEY_CHECKS=0')
         dateData[1] = ((dateData[1].toString().length === 1) ? '0': '') + dateData[1];
         var dateRegion = [dateData[2], dateData[0], dateData[1]].join('-');
         dateRegion += ' 00:00:00';
-        var transparent = (lineData[7] = 'y') ? 1 : 0;
+        //console.log(lineData[7]);
+        var transparent = (lineData[7] === 'y') ? 1 : 0;
 
-        var selectData = [prefix, regionName, regionCode, mobileProper, routeCode, regionId, dateRegion, transparent].join("','");
-
-          console.log(selectData);
-          pool.query("insert into accounting_region values ('" + selectData + "')")
-          .catch(console.log)
+        selectData.push( "('" + [prefix, regionName, regionCode, mobileProper, routeCode, regionId, dateRegion, transparent].join("','") + "')" );
       }
 
     });
   })
-  .catch(console.log);
-  pool.query('SET FOREIGN_KEY_CHECKS=1')
   .then(function() {
-    pool.end();
+    selectData = selectData.join(',');
+    return pool.query("insert into accounting_region values " + selectData + "");
+  })
+  .then(function() {
+    return pool.query('SET FOREIGN_KEY_CHECKS=1');
   })
   .catch(console.log)
+})
+.then(function() {
+  pool.end();
 })
 .catch(console.log);
